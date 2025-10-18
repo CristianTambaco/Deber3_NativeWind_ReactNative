3284243227a20dbb387cf0f45f2dfd8efa166bd7
CristianTambaco <1234cristiant@gmail.com>
2025-10-18 13:23:03 -0500

    modified:   CV-APP/app/preview.tsx 	modified:   CV-APP/tailwind.config.js

--- CV-APP/app/preview.tsx
+++ CV-APP/app/preview.tsx
@@ -1,6 +1,6 @@
 // app/preview.tsx
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View } from "react-native";
 import { useCVContext } from "../context/CVContext";
 import { CVPreview } from "@/components/CVPreview";
 
@@ -8,15 +8,9 @@ export default function PreviewScreen() {
   const { cvData } = useCVContext();
 
   return (
-    <View style={styles.container}>
+    <View className="flex-1 bg-white">
       <CVPreview cvData={cvData} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-  },
-});
