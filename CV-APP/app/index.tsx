b2c6fef004a08af5b88f16e18c61862436d25c4d
CristianTambaco <1234cristiant@gmail.com>
2025-10-18 14:18:49 -0500

    modified:   CV-APP/app/index.tsx

--- CV-APP/app/index.tsx
+++ CV-APP/app/index.tsx
@@ -157,11 +157,17 @@ export default function HomeScreen() {
       {/* Espacio adicional al final para evitar que el último elemento quede oculto */}
       <View style={{ height: 20 }} />
 
-      <View className="flex-1 items-center justify-center bg-white">
+      
+
+      {/* Nativewind! */}
+      
+      {/* <View className="flex-1 items-center justify-center bg-white">
+
       <Text className="text-xl font-bold text-blue-500">
         Welcome to Nativewind!
       </Text>
-    </View>
+
+      </View> */}
 
       
     </ScrollView>
